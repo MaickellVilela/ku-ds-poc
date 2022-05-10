eee1abf00e7c5619178a43898faa2338dc62847d
Maickell Vilela <maickell@me.com>
2022-05-10 16:32:27 -0300

    first chromatic change

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
+
 export default class ButtonComponent implements OnInit {
 
   constructor() { }
@@ -13,3 +14,4 @@ export default class ButtonComponent implements OnInit {
   }
 
 }
+
